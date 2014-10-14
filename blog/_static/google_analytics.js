6162e6bd1b6748b7323ffdbe90d7aade9fae4f27
John Anderson <sontek@gmail.com>
2014-10-14 00:36:50 -0700

    Add back the google analytics static file

--- blog/_static/google_analytics.js
+++ blog/_static/google_analytics.js
@@ -1,8 +1,3 @@
-{% extends "!page.html" %}
-
-{% set script_files = script_files + ["_static/heap.js", "_static/google_analytics.js"] %}
-
-<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -10,5 +5,3 @@
 
   ga('create', 'UA-9376946-2', 'auto');
   ga('send', 'pageview');
-
-</script>
