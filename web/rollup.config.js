2ed1ac9046122761504b945084a69c10a2116ab6
John Anderson <sontek@gmail.com>
2021-01-25 21:15:24 -0400

    start writing the pelican theme

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -71,6 +71,7 @@ export default assets.map((name, index) => {
         copy({
           targets: [
             { src: 'app/index.html', dest: 'dist' },
+            { src: 'app/favicon.ico', dest: 'dist' },
           ]
         }),
       ],
