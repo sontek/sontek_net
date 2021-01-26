2c135ea717aef628bd6c659685b9ce265fdc92e0
John Anderson <sontek@gmail.com>
2021-01-26 15:45:20 -0400

    fix FastAPI proxy

--- api/stkapi/server.py
+++ api/stkapi/server.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from yaml import CLoader as Loader, CDumper as Dumper
 from pathlib import Path
 cwd = Path(__file__)
-app = FastAPI()
+app = FastAPI(root_path='/api')
 
 origins = [
     f"http://{os.environ['STK_HOSTNAME']}/",
