ba641ac60c462c347c3b96122869c8f46ec9975e
John Anderson <sontek@gmail.com>
2021-01-26 20:34:35 -0400

    resume review

---
+++ web/generate_pdf_resume.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://sontek.net/resume', {waitUntil: 'networkidle2'});
+
+  let div_selector_to_remove= ".grid";
+  await page.evaluate((sel) => {
+      var elements = document.querySelectorAll(sel);
+      elements[0].parentNode.removeChild(elements[0]);
+
+      /*
+       * Only want to delete first instance for now
+      for(var i=0; i< elements.length; i++){
+          elements[i].parentNode.removeChild(elements[i]);
+      }
+      */
+  }, div_selector_to_remove)
+
+  await page.pdf({path: 'app/public/sontek_resume.pdf'});
+
+  await browser.close();
+})();
