456fb2d50a9758870fe849ae10c3da5baa7a94a7
John Anderson <sontek@gmail.com>
2022-02-20 21:59:26 -0400

    add panelbear

--- sontek-frontend/pages/_app.js
+++ sontek-frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/global.css";
 import "highlight.js/styles/base16/gruvbox-dark-pale.css";
+import { usePanelbear } from '@panelbear/panelbear-nextjs';
 
 export default function App({ Component, pageProps }) {
+    usePanelbear('JE5hcutbMFz');
     return <Component {...pageProps} />;
 }
