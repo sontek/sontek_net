82074f3cd087fec8ef600fcbe720c1dd2073828b
John Anderson <sontek@gmail.com>
2022-06-26 16:50:23 -0400

    started porting to ts

--- sontek-frontend/pages/_app.tsx
+++ sontek-frontend/pages/_app.tsx
@@ -1,8 +1,10 @@
 import "../styles/global.css";
 import "highlight.js/styles/base16/gruvbox-dark-pale.css";
 import { usePanelbear } from '@panelbear/panelbear-nextjs';
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }) {
+
+export default function App({ Component, pageProps }: AppProps) {
     usePanelbear('JE5hcutbMFz');
     return <Component {...pageProps} />;
 }
