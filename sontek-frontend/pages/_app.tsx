232160f4d7236a2c994a69ccbe03e0a547b95016
John Anderson <sontek@gmail.com>
2023-11-03 00:27:06 -0400

    use easier to read syntax highlighting

--- sontek-frontend/pages/_app.tsx
+++ sontek-frontend/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/global.css";
-// import "highlight.js/styles/base16/gruvbox-dark-pale.css";
+import "highlight.js/styles/base16/gruvbox-dark-pale.css";
 // import "highlight.js/styles/base16/hardcore.css";
 // import "highlight.js/styles/base16/circus.css";
-import "highlight.js/styles/atom-one-dark.css";
+// import "highlight.js/styles/atom-one-dark.css";
 import { usePanelbear } from '@panelbear/panelbear-nextjs';
 import { AppProps } from 'next/app';
 
